0070b6987acf85c23dc97228dadb40a72449d5c3
Paul Roux <paul.roux@phys.ens.fr>
2024-10-23 14:30:57 +0200

    removed bad test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,7 @@
 using BarnesDoubleGamma,
-    Test,
-    LinearAlgebra
-import IterTools: product
+     Test
+#     LinearAlgebra
+# import IterTools: product
 
 @testset "BarnesDoubleGamma.jl" begin
     @testset "shift equations" begin
@@ -16,17 +16,17 @@ import IterTools: product
         @test isapprox(v2/v1, shift, rtol=1e-8)
     end
 
-    function Cref(β, ri, si)
-        return prod(
-            inv(doublegamma(
-                (β+inv(β))/2 + β/2*abs(dot(e, ri)) + inv(β)/2*dot(e, si),
-                β,
-                1e-16
-            ))
-            for e in product((-1, 1), (-1, 1), (-1, 1))
-        )
-    end
-    β = sqrt(3/4)
-    β2P(r, s) = (r*β^2-1)
-    println(Cref(β, (0, 0, 0), (β2P(1, 2), β2P(1, 2), β2P(1, 2))))
+    # function Cref(β, ri, si)
+    #     return prod(
+    #         inv(doublegamma(
+    #             (β+inv(β))/2 + β/2*abs(dot(e, ri)) + inv(β)/2*dot(e, si),
+    #             β,
+    #             1e-16
+    #         ))
+    #         for e in product((-1, 1), (-1, 1), (-1, 1))
+    #     )
+    # end
+    # β = sqrt(3/4)
+    # β2P(r, s) = (r*β^2-1)
+    # println(Cref(β, (0, 0, 0), (β2P(1, 2), β2P(1, 2), β2P(1, 2))))
 end
